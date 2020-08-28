9e044a35a2c79062305152cb32040ba60eaaffe6
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-08-28 15:13:05 +0200

    add webcam fetch

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
-import { File } from '@ionic-native/file/ngx';
-
 import '@capacitor-community/http';
 import { Plugins, FilesystemDirectory } from '@capacitor/core';
-const { Http } = Plugins;
+const { Http, Filesystem } = Plugins;
+
+import { HTTP } from '@ionic-native/http/ngx';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() {}
+  constructor(private httpNative: HTTP, private platform: Platform) {}
 
   async getPSNStatus() {
     const response = await Http.request({
@@ -51,32 +52,37 @@ export class ApiService {
   }
 
   async getLatestWebcam() {
-    let result = null;
-    const ret = await Http.downloadFile({
-      url: environment.apiUrl + '/webcam?id=psn',
-      filePath: 'webcam.jpg',
-      fileDirectory: FilesystemDirectory.Data
-    });
+    if (this.platform.is('capacitor')) {
+      const folder = await Filesystem.getUri({
+        directory: FilesystemDirectory.Data,
+        path: ''
+      });
 
-    await new Promise((resolve, reject) => {
-      console.log(ret);
+      const path = folder + '/webcam.jpg';
+      await this.httpNative.downloadFile(environment.apiUrl + '/webcam?id=psn', null, null, path);
+      const image = ( window as any).Ionic.WebView.convertFileSrc(path);
+      return image + '?date=' + new Date().getTime();
+    } else {
+      let result = null;
+      const ret = await Http.downloadFile({
+        url: environment.apiUrl + '/webcam?id=psn',
+        filePath: 'webcam.jpg',
+        fileDirectory: FilesystemDirectory.Data
+      });
 
-      if (ret.blob) {
-        const reader = new FileReader();
-        console.log(reader);
-
-        reader.onload = () => {
-          console.log(reader);
-
-          result = reader.result;
-          resolve();
-        };
-        reader.readAsDataURL(ret.blob);
-      } else {
-        reject();
-      }
-    });
-    return result;
+      await new Promise((resolve, reject) => {
+        if (ret.blob) {
+          const reader = new FileReader();
+          reader.onload = () => {
+            result = reader.result;
+            resolve();
+          };
+          reader.readAsDataURL(ret.blob);
+        } else {
+          reject();
+        }
+      });
+      return result;
+    }
   }
-
 }
