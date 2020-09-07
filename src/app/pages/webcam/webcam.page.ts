14b12d477aad1aae3249742b90c041743ff73d80
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-07 13:40:13 +0200

    feat(webcam): re add update interval

--- src/app/pages/webcam/webcam.page.ts
+++ src/app/pages/webcam/webcam.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+
+import { environment } from '../../../environments/environment';
 import { ApiService } from '../../services/api.service';
 
 @Component({
@@ -7,14 +9,19 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './webcam.page.html',
   styleUrls: ['./webcam.page.scss'],
 })
-export class WebcamPage implements OnInit {
+export class WebcamPage implements OnInit, OnDestroy {
 
   public image: any;
   public date: Date;
+  public updateInterval: any;
 
   constructor(private modalController: ModalController, private api: ApiService) { }
 
   ngOnInit() {
+    this.updateInterval = setInterval(() => {
+      this.getWebcam();
+    }, environment.webcamUpdateInterval);
+
     this.getWebcam();
   }
 
@@ -32,4 +39,7 @@ export class WebcamPage implements OnInit {
     this.modalController.dismiss();
   }
 
+  ngOnDestroy() {
+    clearInterval(this.updateInterval);
+  }
 }
