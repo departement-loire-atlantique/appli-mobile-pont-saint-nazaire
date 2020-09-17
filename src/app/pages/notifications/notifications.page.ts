20ae6b70deb56747d129f4ea2386e3e633460aca
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-17 11:31:37 +0200

    fix merge issue

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { langFr } from 'src/app/models/constantesCD44';
+import { ErrorService } from 'src/app/services/error.service';
 
 import { NotificationsService } from '../../services/notifications.service';
-<<<<<<< HEAD
-import { ErrorService } from 'src/app/services/error.service';
-import { langFr } from 'src/app/models/constantesCD44';
-=======
 import { StorageService } from '../../services/storage.service';
->>>>>>> 5da1e8a8bbbf85b728ca244f4e0e87189cd8c091
 
 @Component({
   selector: 'app-notifications',
@@ -17,9 +14,11 @@ export class NotificationsPage implements OnInit {
 
   public isSubscribed: boolean;
 
-  constructor(private storageService: StorageService,
-              private notificationService: NotificationsService,
-              private erroService: ErrorService) { }
+  constructor(
+    private storageService: StorageService,
+    private notificationService: NotificationsService,
+    private erroService: ErrorService
+  ) { }
 
   ngOnInit() {
     this.storageService.get(this.notificationService.STORAGEKEY).then((value: boolean) => {
